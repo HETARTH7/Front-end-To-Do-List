d57e6d6277ff83b2762bef33b275eb0c492f8300
unknown <hetarth002@gmail.com>
2022-06-27 12:24:26 +0530

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
 
+  function addItem() {
+    setItems((prevItem) => [...prevItem, inputText]);
+    setInputText("");
+  }
   return (
     <div className="container">
       <div className="heading">
@@ -14,13 +20,15 @@ function App() {
       </div>
       <div className="form">
         <input onChange={handleChange} type="text" value={inputText} />
-        <button>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
